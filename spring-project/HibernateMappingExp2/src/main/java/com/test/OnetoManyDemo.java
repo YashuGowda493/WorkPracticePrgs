b941d99d7a0bfda5a328bbad06a259ac5be11635
YashuGowda493 <159759056+YashuGowda493@users.noreply.github.com>
2026-03-26 17:17:02 +0530

    Updated attributes

--- spring-project/HibernateMappingExp2/src/main/java/com/test/OnetoManyDemo.java
+++ spring-project/HibernateMappingExp2/src/main/java/com/test/OnetoManyDemo.java
@@ -20,19 +20,29 @@ public class OnetoManyDemo {
 		Order o1=new Order();
 		o1.setO_name("Biryani");
 		o1.setO_price(200);
+		o1.setO_quant(5);
 		
 		Order o2=new Order();
 		o2.setO_name("Kabab");
 		o2.setO_price(150);
+		o2.setO_quant(4);
 		
 		List<Order> list=Arrays.asList(o1,o2);
 		
 		User user=new User();
 		user.setU_name("ABC");
+		user.setU_location("xxx");
+		user.setU_city("Blore");
+		user.setU_pincode(560057);
+		user.setPhone(3456789021l);
 		user.setOdr(list);
 		
 		User user2=new User();
-		user2.setU_name("XYZ");	
+		user2.setU_name("XYZ");
+		user2.setU_location("zzz");
+		user2.setU_city("mlore");
+		user2.setU_pincode(580017);
+		user2.setPhone(9999944441l);
 		user2.setOdr(list);
 		
 		o1.setUser(user);
